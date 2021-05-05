16b685c105453ecfcf3f5b91e4859d2b6eb1d114
Wilfer Cortes U <wcortes@stt.com.co>
2021-05-05 06:08:10 -0500

    docs created

---
+++ docs/precache-manifest.d7c8baa14c5e8f4c229bb0a1a7642ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d5131d90f2cbb590b5e40347f133ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "574ef4c9e40ac00c7126",
+    "url": "/static/css/main.d71d850a.chunk.css"
+  },
+  {
+    "revision": "c6a93981b4deb6fc60df",
+    "url": "/static/js/2.569b9471.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.569b9471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574ef4c9e40ac00c7126",
+    "url": "/static/js/main.d1ce9fdc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
